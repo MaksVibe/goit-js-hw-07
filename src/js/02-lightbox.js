bb681f07286e1188032f634a4a3437a9bf0e72ed
MaksVibe <parunovmaksym@gmail.com>
2021-11-11 19:45:22 +0200

    2 Task

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,34 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+let gallery = document.querySelector(".gallery");
 
+const liMarkup = galleryItems
+  .map(({ preview, description, original }) => {
+    //   Add item
+    const liRef = document.createElement("li");
+    liRef.classList.add("gallery__item");
+    gallery.append(liRef);
+    // Add link
+    const linkRef = document.createElement("a");
+    linkRef.classList.add("gallery__link");
+    linkRef.setAttribute("href", original);
+    liRef.append(linkRef);
+    // Add img
+    const imgRef = document.createElement("img");
+    imgRef.classList.add("gallery__image");
+    imgRef.setAttribute("data-source", original);
+    imgRef.src = preview;
+    imgRef.alt = description;
+    imgRef.classList.add("img");
+    linkRef.append(imgRef);
+  })
+  .join("");
+
+gallery.insertAdjacentHTML("afterbegin", liMarkup);
+
+new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
 console.log(galleryItems);
