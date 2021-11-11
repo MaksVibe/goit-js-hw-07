e228dccf4e72eb7662ef66a5694031d5d49d7a15
MaksVibe <parunovmaksym@gmail.com>
2021-11-11 18:13:40 +0200

    Add Task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,37 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const gallery = document.querySelector(".gallery");
 
-console.log(galleryItems);
+const liMarkup = galleryItems
+  .map(({ preview, description, original }) => {
+    //   Add item
+    const liRef = document.createElement("li");
+    liRef.classList.add("gallery__item");
+    gallery.append(liRef);
+    // Add link
+    const linkRef = document.createElement("a");
+    linkRef.classList.add("gallery__link");
+    linkRef.setAttribute("href", original);
+    liRef.append(linkRef);
+    // Add img
+    const imgRef = document.createElement("img");
+    imgRef.classList.add("gallery__image");
+    imgRef.setAttribute("data-source", original);
+    imgRef.src = preview;
+    imgRef.alt = description;
+    imgRef.classList.add("img");
+    linkRef.append(imgRef);
+  })
+  .join("");
+
+gallery.insertAdjacentHTML("afterbegin", liMarkup);
+gallery.addEventListener("click", (e) => {
+  e.preventDefault();
+  basicLightbox
+    .create(
+      `
+		<img src="${e.target.closest("a").href}">
+	`
+    )
+    .show();
+});
